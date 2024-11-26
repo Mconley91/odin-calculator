8cc6ceef5f26b07493e6c8ad4519e9ec078ab762
mconley91 <matthew.e.conley@gmail.com>
2024-11-26 16:40:57 -0500

    add input functionality for all buttons

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const screen = document.querySelector('#screen');
+const displayScreen = document.querySelector('#screen');
 const btnZero = document.querySelector('#zero');
 const btnOne = document.querySelector('#one');
 const btnTwo = document.querySelector('#two');
@@ -14,3 +14,23 @@ const btnSubtract = document.querySelector('#subtract');
 const btnMultiply = document.querySelector('#multiply');
 const btnDivide = document.querySelector('#divide');
 const btnEquals = document.querySelector('#equals');
+const btnClear = document.querySelector('#clear');
+
+btnZero.addEventListener('click',()=>{displayScreen.textContent += '0'});
+btnOne.addEventListener('click',()=>{displayScreen.textContent += '1'});
+btnTwo.addEventListener('click',()=>{displayScreen.textContent += '2'});
+btnThree.addEventListener('click',()=>{displayScreen.textContent += '3'});
+btnFour.addEventListener('click',()=>{displayScreen.textContent += '4'});
+btnFive.addEventListener('click',()=>{displayScreen.textContent += '5'});
+btnSix.addEventListener('click',()=>{displayScreen.textContent += '6'});
+btnSeven.addEventListener('click',()=>{displayScreen.textContent += '7'});
+btnEight.addEventListener('click',()=>{displayScreen.textContent += '8'});
+btnNine.addEventListener('click',()=>{displayScreen.textContent += '9'});
+
+btnAdd.addEventListener('click',()=>{displayScreen.textContent += '+'});
+btnSubtract.addEventListener('click',()=>{displayScreen.textContent += '-'});
+btnMultiply.addEventListener('click',()=>{displayScreen.textContent += '*'});
+btnDivide.addEventListener('click',()=>{displayScreen.textContent += '/'});
+btnEquals.addEventListener('click',()=>{displayScreen.textContent += '='});
+
+btnClear.addEventListener('click',()=>{displayScreen.textContent = ''});
